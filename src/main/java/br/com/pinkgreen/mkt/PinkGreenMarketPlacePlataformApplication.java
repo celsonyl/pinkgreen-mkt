1a418ac32e60de0b5bdbcab2b99bd41644895608
Bruno Ventura <brunoventura80@gmail.com>
2021-04-26 20:59:38 -0300

    First commit

---
+++ src/main/java/br/com/pinkgreen/mkt/PinkGreenMarketPlacePlataformApplication.java
@@ -0,0 +1,13 @@
+package br.com.pinkgreen.mkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkGreenMarketPlacePlataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkGreenMarketPlacePlataformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pinkgreen/mkt/PinkGreenMarketPlacePlataformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pinkgreen.mkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkGreenMarketPlacePlataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
