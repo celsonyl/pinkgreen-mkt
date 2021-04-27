c1ffa4881596d6b2a386de91348360660df3aa15
Celso Bertolotto <celsonyl@hotmail.com>
2021-04-26 21:25:32 -0300

    docker

--- src/main/java/br/com/pinkgreen/mkt/PinkGreenMarketPlacePlataformApplication.java
+++ src/main/java/br/com/pinkgreen/mkt/PinkGreenMarketPlacePlataformApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PinkGreenMarketPlacePlataformApplication {
+public class PinkGreenMarketPlacePlataformApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PinkGreenMarketPlacePlataformApplication.class, args);
