08d5ef338f07b1636f4ff6b1d41d00db184f0144
Bruno Ventura <brunoventura80@gmail.com>
2021-04-27 23:02:06 -0300

    Creating TODOs

--- src/main/java/br/com/pinkgreen/mkt/usecase/CheckoutOrderUseCase.java
+++ src/main/java/br/com/pinkgreen/mkt/usecase/CheckoutOrderUseCase.java
@@ -4,11 +4,16 @@ package br.com.pinkgreen.mkt.usecase;
 import br.com.pinkgreen.mkt.domain.OrderDomain;
 
 public class CheckoutOrderUseCase {
+    // TODO - Consultar catalogo para validar dados do produto recebido -> futuro
+    // TODO - Validar customerId recebido (se customerId existe na base) -> futuro
 
     public OrderDomain execute(OrderDomain orderDomain) {
-        // TODO - Calcular valor do pagamento final de acordo com os produtos
-        // TODO - Consultar catalogo para validar dados do produto recebido
-        // TODO - Validar customerId recebido (se customerId existe na base)
+        // TODO - Codar escrevendo testes unitários!
+        // TODO - Calcular valor do pagamento final de acordo com os produtos -> criar usecase?
+        // TODO - Persistir pedido no banco de dados
+        // TODO - Postar pedido criado em uma fila no rabbit como payload o orderId e um header com o status de ORDER_CREATED
+        // TODO - Retornar OrderDomain com orderId preenchido
+
         return null;
     }
 }
