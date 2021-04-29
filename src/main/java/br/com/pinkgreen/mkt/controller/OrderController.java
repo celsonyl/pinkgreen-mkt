a08ef20d65187b1070055194ec974426190616f4
Bruno Ventura <brunoventura80@gmail.com>
2021-04-28 22:05:51 -0300

    Adjusting order controller

--- src/main/java/br/com/pinkgreen/mkt/controller/OrderController.java
+++ src/main/java/br/com/pinkgreen/mkt/controller/OrderController.java
@@ -6,23 +6,27 @@ import br.com.pinkgreen.mkt.controller.translator.CheckoutRequestMapperImpl;
 import br.com.pinkgreen.mkt.domain.OrderDomain;
 import br.com.pinkgreen.mkt.usecase.CheckoutOrderUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @Component
-@RestController("/order")
+@RestController
+@RequestMapping("/order")
 @RequiredArgsConstructor
 public class OrderController {
 
     private final CheckoutOrderUseCase checkoutOrderUseCase;
 
     @PostMapping
-    public CheckoutOrderResponse checkout(@Valid OrderRequest orderRequest) {
+    public ResponseEntity<CheckoutOrderResponse> checkout(@Valid @RequestBody OrderRequest orderRequest) {
         OrderDomain orderDomain = new CheckoutRequestMapperImpl().checkoutRequestToCheckout(orderRequest);
         checkoutOrderUseCase.execute(orderDomain);
-        return null;
+        return ResponseEntity.ok().body(CheckoutOrderResponse.builder().build());
     }
 }
