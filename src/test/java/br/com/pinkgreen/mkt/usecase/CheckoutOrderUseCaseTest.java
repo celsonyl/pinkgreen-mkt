8b326d7f39d0541eb9795fedea1fa93d86a66f4e
brunovnasci <brunoventura80@gmail.com>
2021-05-11 23:08:39 -0300

    Adjusting unit tests

--- src/test/java/br/com/pinkgreen/mkt/usecase/CheckoutOrderUseCaseTest.java
+++ src/test/java/br/com/pinkgreen/mkt/usecase/CheckoutOrderUseCaseTest.java
@@ -21,23 +21,37 @@ import static org.mockito.Mockito.*;
 class CheckoutOrderUseCaseTest {
 
     private final ArgumentCaptor<OrderDomain> orderDomainArgument = forClass(OrderDomain.class);
+    private final ArgumentCaptor<OrderDomain> publishOrderDomainArgument = forClass(OrderDomain.class);
+    private final ArgumentCaptor<PaymentDomain> publishPaymentDomainArgument = forClass(PaymentDomain.class);
     private final CheckoutOrderGateway checkoutOrderGateway = mock(CheckoutOrderGateway.class);
     private final PublishOrderToProcessPayment publishOrderToProcessPayment = mock(PublishOrderToProcessPayment.class);
-    private final CheckoutOrderUseCase checkoutOrderUseCase = new CheckoutOrderUseCase(checkoutOrderGateway,publishOrderToProcessPayment);
+    private final CheckoutOrderUseCase checkoutOrderUseCase = new CheckoutOrderUseCase(checkoutOrderGateway, publishOrderToProcessPayment);
 
     @Test
     void shouldCheckoutOrderSuccessfully() {
         OrderDomain orderDomain = getOrder();
+        when(checkoutOrderGateway.execute(any())).thenReturn(getPersistedOrder());
 
         checkoutOrderUseCase.execute(orderDomain, orderDomain.getPaymentData());
 
         verify(checkoutOrderGateway).execute(orderDomainArgument.capture());
-
-        when(checkoutOrderGateway.execute(orderDomain)).thenReturn(getPersistedOrder());
+        verify(publishOrderToProcessPayment, times(1)).publish(publishOrderDomainArgument.capture(), publishPaymentDomainArgument.capture());
 
         OrderDomain orderDomainArgumentValue = orderDomainArgument.getValue();
+        OrderDomain publishOrderDomainArgumentValue = publishOrderDomainArgument.getValue();
+        PaymentDomain publishPaymentDomainArgumentValue = publishPaymentDomainArgument.getValue();
+
+        assertEquals("1234", publishOrderDomainArgumentValue.getId());
         assertEquals(ORDER_CREATED, orderDomainArgumentValue.getStatus());
         assertEquals(9066.00, orderDomainArgumentValue.getPaymentData().getAmount());
+        assertEquals("1111222233334444", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("cardNumber"));
+        assertEquals("123", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("cvv"));
+        assertEquals("01/22", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("validationDate"));
+        assertEquals("947.229.723-46", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("document"));
+        assertEquals("Elza Luna Rocha", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("ownerName"));
+        assertEquals("21/05/2000", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("birthday"));
+        assertEquals("+55 19 99999-9999", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("phone"));
+        assertEquals("test@test.com.br", publishPaymentDomainArgumentValue.getPaymentMethodProperties().get("email"));
     }
 
     public OrderDomain getOrder() {
@@ -132,12 +146,19 @@ class CheckoutOrderUseCaseTest {
                 .sku("99999999")
                 .build();
 
+        PaymentDomain paymentDomain = PaymentDomain.builder()
+                .paymentMethod(PaymentMethod.CREDIT_CARD)
+                .paymentAddress(addressDomain)
+                .amount(9066.00)
+                .build();
+
         return OrderDomain.builder()
                 .id("1234")
                 .customerData(customerDomain)
                 .status(ORDER_CREATED)
                 .shippingData(shippingDataDomain)
                 .productList(Arrays.asList(productDomain, productDomain2))
+                .paymentData(paymentDomain)
                 .createdAt(Instant.now())
                 .updatedAt(Instant.now())
                 .build();
@@ -162,12 +183,67 @@ class CheckoutOrderUseCaseTest {
                 paymentMethodProperties.put("ownerName", "Elza Luna Rocha");
                 paymentMethodProperties.put("phone", "+55 19 99999-9999");
                 paymentMethodProperties.put("email", "test@test.com.br");
+                break;
         }
 
         return PaymentDomain.builder()
                 .paymentAddress(addressDomain)
                 .paymentMethod(paymentMethod)
                 .paymentMethodProperties(paymentMethodProperties)
+                .build();
+    }
+
+    public OrderDomain getCheckoutOrderGatewayReturn() {
+        CustomerDomain customerDomain = CustomerDomain.builder()
+                .id("3zyaa7ge0zh64rgooswxbzkmfucl3k")
+                .document("947.229.723-46")
+                .name("Elza")
+                .lastname("Luna Rocha")
+                .email("test@test.com.br")
+                .phone("+55 (19) 99999-9999")
+                .build();
+
+        AddressDomain addressDomain = AddressDomain.builder()
+                .country("Brasil")
+                .state("SP")
+                .city("Limeira")
+                .neighborhood("Centro")
+                .street("Rua Boa Morte")
+                .number("380")
+                .zipcode("13480-180")
+                .complement("")
+                .phone("(19) 99999-9999")
+                .build();
+
+        ShippingDataDomain shippingDataDomain = ShippingDataDomain.builder()
+                .deliveryDays(6)
+                .freightPrice(19.79)
+                .address(addressDomain)
+                .build();
+
+        ProductDomain productDomain = ProductDomain.builder()
+                .name("Samsung Galaxy S21 Cinza")
+                .price(3859.00)
+                .quantity(1000)
+                .sku("888888888")
+                .build();
+
+        ProductDomain productDomain2 = ProductDomain.builder()
+                .name("Apple iPhone 12 Roxo")
+                .price(5207.00)
+                .quantity(1000)
+                .sku("99999999")
+                .build();
+
+        PaymentDomain paymentDomain = getPaymentByPaymentMethod(addressDomain, PaymentMethod.CREDIT_CARD);
+        paymentDomain.setAmount(9066.00);
+
+        return OrderDomain.builder()
+                .status(ORDER_CREATED)
+                .customerData(customerDomain)
+                .shippingData(shippingDataDomain)
+                .productList(Arrays.asList(productDomain, productDomain2))
+                .paymentData(paymentDomain)
                 .build();
     }
 }
