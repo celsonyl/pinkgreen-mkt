b2b6a17d4d01cd3854179d1b588af85d1c4d140f
brunovnasci <brunoventura80@gmail.com>
2021-06-01 00:28:08 -0300

    Removing rabbitmq deprecated implementation

--- src/main/java/br/com/pinkgreen/mkt/gateway/rabbitmq/PublishOrderToProcessPaymentRabbitImpl.java
+++ src/main/java/br/com/pinkgreen/mkt/gateway/rabbitmq/PublishOrderToProcessPaymentRabbitImpl.java
@@ -4,24 +4,24 @@ import br.com.pinkgreen.mkt.domain.OrderDomain;
 import br.com.pinkgreen.mkt.domain.PaymentDomain;
 import br.com.pinkgreen.mkt.gateway.PublishOrderToProcessPayment;
 import br.com.pinkgreen.mkt.gateway.rabbitmq.model.ProcessOrderPaymentMessage;
-import br.com.pinkgreen.mkt.gateway.rabbitmq.source.ProcessOrderPaymentSource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+import static br.com.pinkgreen.mkt.gateway.rabbitmq.supplier.PublishOrderToProcessPaymentSupplier.OUTPUT;
+
 @Component
 @RequiredArgsConstructor
-@EnableBinding(ProcessOrderPaymentSource.class)
 public class PublishOrderToProcessPaymentRabbitImpl implements PublishOrderToProcessPayment {
 
-    private final ProcessOrderPaymentSource processOrderPaymentSource;
+    private final StreamBridge streamBridge;
 
     public void publish(OrderDomain orderDomain, PaymentDomain paymentDomain) {
         ProcessOrderPaymentMessage processOrderPaymentMessage = ProcessOrderPaymentMessage.builder()
                 .orderId(orderDomain.getId())
                 .paymentDomain(paymentDomain)
                 .build();
-        processOrderPaymentSource.sendMessage().send(MessageBuilder.withPayload(processOrderPaymentMessage).build());
+        streamBridge.send(OUTPUT, MessageBuilder.withPayload(processOrderPaymentMessage).build());
     }
 }
--- src/main/java/br/com/pinkgreen/mkt/listener/ProcessOrderPaymentListener.java
+++ src/main/java/br/com/pinkgreen/mkt/listener/ProcessOrderPaymentListener.java
@@ -1,17 +1,20 @@
 package br.com.pinkgreen.mkt.listener;
 
 import br.com.pinkgreen.mkt.gateway.rabbitmq.model.ProcessOrderPaymentMessage;
-import br.com.pinkgreen.mkt.listener.sink.ProcessOrderPaymentSink;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 @Slf4j
-@EnableBinding(ProcessOrderPaymentSink.class)
+@Component
 public class ProcessOrderPaymentListener {
 
-    @StreamListener(ProcessOrderPaymentSink.INPUT)
-    public void receiver(ProcessOrderPaymentMessage processOrderPaymentMessage){
-        log.info("Pedido numero {} recebido para processamento de pagamento",processOrderPaymentMessage.getOrderId());
+    @Bean
+    public Consumer<ProcessOrderPaymentMessage> processOrderPaymentConsumer() {
+        return message -> {
+            log.info("Pedido numero {} recebido para processamento de pagamento", message.getOrderId());
+        };
     }
 }
