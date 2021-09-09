3a6623bc8d59b7312f79c6ffc1211e2d6af334a5
Celso Bertolotto <celsonyl@hotmail.com>
2021-09-09 15:23:53 -0300

    throw exception if does not have orders to ship

--- src/main/java/br/com/pinkgreen/mkt/usecase/GetAllOrdersReadyToShipUseCase.java
+++ src/main/java/br/com/pinkgreen/mkt/usecase/GetAllOrdersReadyToShipUseCase.java
@@ -2,6 +2,7 @@ package br.com.pinkgreen.mkt.usecase;
 
 import br.com.pinkgreen.mkt.domain.OrderDomain;
 import br.com.pinkgreen.mkt.domain.enums.OrderStatus;
+import br.com.pinkgreen.mkt.domain.exception.ObjectNotFoundException;
 import br.com.pinkgreen.mkt.gateway.GetAllOrdersByStatusGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,11 @@ public class GetAllOrdersReadyToShipUseCase {
     private final GetAllOrdersByStatusGateway getAllOrdersByStatusGateway;
 
     public List<OrderDomain> execute() {
-        return getAllOrdersByStatusGateway.execute(OrderStatus.ORDER_STOCK_RESERVED);
+        List<OrderDomain> execute = getAllOrdersByStatusGateway.execute(OrderStatus.ORDER_STOCK_RESERVED);
+
+        if (execute.size() == 0) {
+            throw new ObjectNotFoundException("Does not have orders ready to go");
+        }
+        return execute;
     }
 }
