b914e0f6848d569d6595db588e3ae37c1664fbab
Bruno Ventura <brunoventura80@gmail.com>
2023-03-30 15:56:11 -0300

    health: catalog updated

--- src/main/resources/db/migrations/V1.5.0__INSERT_FAVORITE_PRODUCTS.sql
+++ src/main/resources/db/migrations/V1.5.0__INSERT_FAVORITE_PRODUCTS.sql
@@ -1,8 +1,8 @@
 insert into FAVORITE_PRODUCTS (USER_ID, SKU_CODE)
-values ('64b5c9e4-8740-41f6-b66f-279631dff64e', 'AFRQF5-DWW-12EN');
+values ('64b5c9e4-8740-41f6-b66f-279631dff64e', 'samsung-smart-tv-50au7700-preta');
 
 insert into FAVORITE_PRODUCTS (USER_ID, SKU_CODE)
-values ('64b5c9e4-8740-41f6-b66f-279631dff64e', 'A513-54-57EN');
+values ('64b5c9e4-8740-41f6-b66f-279631dff64e', 'acer-aspire-5-core-i3-256gb-4gb-ram');
 
 insert into FAVORITE_PRODUCTS (USER_ID, SKU_CODE)
-values ('e1503b98-4e3c-41fb-9faf-bc4c80368796', 'A513-54-57EN');
+values ('e1503b98-4e3c-41fb-9faf-bc4c80368796', 'acer-aspire-5-core-i3-256gb-4gb-ram');
